df5e910a74f2da5e9b93952b20bbb567f323baa3
Sharath Kumar A <sharathkannanakz@gmail.com>
2023-03-15 16:03:09 +0530

    update1

--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/listen',
+mongoose.connect('mongodb+srv://sharathkannanakz:sharadha@cluster0.lotufey.mongodb.net/listen',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
